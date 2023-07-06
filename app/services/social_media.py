82e878618be75dc976576e05abac70bf15c5516b
Daniel Vinicius <viniccius774@gmail.com>
2023-07-06 10:33:04 -0300

    nova conexão com o twitter

--- app/services/social_media.py
+++ app/services/social_media.py
@@ -4,19 +4,33 @@ from dotenv import load_dotenv
 load_dotenv()
 from os import environ
 
+consumer_key = environ['consumer_key']
+
+consumer_secret = environ['consumer_secret']
+
+access_token = environ['access_token']
+
+access_token_secret = environ['access_token_secret']
+
 # Lib do twitter
 import tweepy
-twitter_client = tweepy.Client(environ['BEARER_TOKEN'])
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Create the API object
+api = tweepy.API(auth)
 
 class SocialMedia:
     def __init__(self):
-        self.client = twitter_client
+        self.client = api
 
     def fetch_from_twitter(self, tag):
         try:
-            tweets = twitter_client.search_recent_tweets(tag)
-            id_primeiro_tweet = tweets.data[0].id
-            corpo_do_tweet = self.client.get_tweet(id_primeiro_tweet).data.text
-            return [id_primeiro_tweet, corpo_do_tweet]
+            # ID do Brasil
+            tweets = self.client.search_tweets(tag, geocode="-14.2350,-51.9253,2000km")
+            id_primeiro_tweet = tweets[0].id
+            corpo_do_tweet = api.get_status(id_primeiro_tweet)
+            print(corpo_do_tweet.text, id_primeiro_tweet)
+            return [id_primeiro_tweet, corpo_do_tweet.text]
         except:
             print('Erro ao buscar tweets.')
